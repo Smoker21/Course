3babd4594efbb81794d7caff4aa0dbc0c2c4ab4e
smoker21 <chen.lance@gmail.com>
2014-11-09 00:08:14 +0800

    update

--- Course01/MvnFileFinder/src/test/java/com/rainsoft/training/course01/FileList03Test.java
+++ Course01/MvnFileFinder/src/test/java/com/rainsoft/training/course01/FileList03Test.java
@@ -4,14 +4,23 @@ import java.io.File;
 import java.util.Collection;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FileList03Test {
 
+	String testPath;
+
+	@Before
+	public void setup() {
+		testPath = ClassLoader.getSystemResource("testfolder").getPath();
+	}
+
 	@Test
 	public void testGetFiles() {
 		final FileList03 f03 = new FileList03();
-		final Collection<File> files = f03.getFiles("testFolder/");
+
+		final Collection<File> files = f03.getFiles("testPath");
 		Assert.assertEquals(2, files.size());
 		final File f = new File("testFolder/testme.md");
 		Assert.assertEquals(f.getAbsolutePath(), ((File) files.toArray()[0]).getAbsolutePath());
