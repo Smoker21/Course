1feb0ecf4ed788034e24556b03afc4ed90988d5c
smoker21 <chen.lance@gmail.com>
2014-11-08 10:39:39 +0800

    Fist commit

---
+++ Course01/FileFinder/src/com/rainsoft/course01/FileList02.java
@@ -0,0 +1,36 @@
+package com.rainsoft.course01;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Get File Version 2.
+ *
+ * @author Lance
+ *
+ */
+public class FileList02 {
+	public List<String> getFiles(final String path) {
+		final File folder = new File(path);
+		final File[] listOfFiles = folder.listFiles();
+		final List<String> fileNameList = new ArrayList<String>();
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				fileNameList.add(listOfFiles[i].getAbsolutePath());
+			} else if (listOfFiles[i].isDirectory()) {
+				fileNameList.add(listOfFiles[i].getAbsolutePath());
+			}
+		}
+		return fileNameList;
+	}
+
+	public final static void main(final String... args) {
+		final FileList02 fileList = new FileList02();
+		final List<String> fileNameList = fileList.getFiles("c:/");
+		for (final String name : fileNameList) {
+			System.out.println(name);
+		}
+
+	}
+}
---
+++ Course01/FileFinder/test/com/rainsoft/course01/FileList02Test.java
@@ -0,0 +1,26 @@
+package com.rainsoft.course01;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FileList02Test {
+
+	@Before
+	public void setupTest() {
+
+	}
+
+	@Test
+	public void testGetFiles() {
+		final FileList02 f02 = new FileList02();
+		final List<String> list = f02.getFiles("testFolder/");
+		Assert.assertEquals(1, list.size());
+		final File f = new File("testFolder/testme.md");
+		Assert.assertEquals(f.getAbsolutePath(), list.get(0));
+	}
+
+}
