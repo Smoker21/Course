90f1538dddcc8070813eb9a83a71e91afa72a1f6
smoker21 <chen.lance@gmail.com>
2016-11-09 00:12:02 +0800

    update to springboot 1.4.1

--- Course07/SpringBootAdminSample/src/main/java/com/rainty/sample/course7/SpringBootAdminApplication.java
+++ Course07/SpringBootAdminSample/src/main/java/com/rainty/sample/course7/SpringBootAdminApplication.java
@@ -2,12 +2,14 @@ package com.rainty.sample.course7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @Configuration
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableAdminServer
 public class SpringBootAdminApplication {
 	public static void main(final String[] args) {
